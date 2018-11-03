c8b7c0faa3fdcf6005ff5c5d30c464b6063f8c7f
Rayhan Nasir <34342995+RayhanNasir@users.noreply.github.com>
2018-11-03 13:25:20 +0600

    Add files via upload

---
+++ infinite_argument.py
@@ -0,0 +1,6 @@
+#it's take infinite number of argument
+
+def infinite_argument(*argument):
+    for people in argument:
+        print("this person is "+ people)
+infinite_argument('rayhan','nasim','ibrahim','toriq','arif')
